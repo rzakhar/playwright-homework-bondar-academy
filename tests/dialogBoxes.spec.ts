6c1d69b37a505a35af93f393dc253a42f091f08f
Roman Zakharov <romanzakharov329@gmail.com>
2025-11-13 13:50:25 +0200

    Simplify last row locators

--- tests/dialogBoxes.spec.ts
+++ tests/dialogBoxes.spec.ts
@@ -7,7 +7,7 @@ test('Add and delete pet type', async ({ page }) => {
         await page.getByRole('link', { name: 'Pet Types' }).click();
         await expect(page.getByRole('heading', { name: 'Pet Types' })).toBeVisible();
     });
-    const lastRow = page.locator('table#pettypes').getByRole('row').last();
+    const lastTableTextBox = page.getByRole("table").getByRole('textbox').last();
     await test.step('Add new pet type', async () => {
         await page.getByRole('button', { name: 'Add' }).click();
         await expect(page.getByRole('heading', { name: 'New Pet Type' })).toBeVisible();
@@ -15,15 +15,14 @@ test('Add and delete pet type', async ({ page }) => {
         await expect(page.locator('input#name')).toBeVisible();
         await page.locator('input#name').fill('pig');
         await page.getByRole('button', { name: 'Save' }).click();
-
-        await expect(lastRow.locator('input')).toHaveValue('pig');
+        await expect(lastTableTextBox).toHaveValue('pig');
     });
     await test.step('Delete the added pet type', async () => {
         page.on('dialog', async dialog => {
             expect(dialog.message()).toContain('Delete the pet type?');
             await dialog.accept();
         });
-        await lastRow.getByRole('button', { name: 'Delete' }).click();
-        await expect(lastRow.locator('input')).not.toHaveValue('pig');
+        await page.getByRole('button', { name: 'Delete' }).last().click();
+        await expect(lastTableTextBox).not.toHaveValue('pig');
     });
 })
