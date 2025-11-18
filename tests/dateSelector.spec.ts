81387c915635b6eb4fa4c2214f0f82c7aada8cae
Roman Zakharov <romanzakharov329@gmail.com>
2025-11-18 12:53:33 +0200

    Refactor date selector tests for clarity and robustness

--- tests/dateSelector.spec.ts
+++ tests/dateSelector.spec.ts
@@ -18,7 +18,7 @@ test.describe('Date Selectors Tests', () => {
         await page.getByRole('button', { name: 'Add New Pet' }).click();
         const petNameTextBox = page.getByRole('textbox', { name: 'Name' });
         await petNameTextBox.fill('Tom');
-        await expect(page.locator("div.col-sm-10", { has: petNameTextBox }).locator('span')).toHaveClass("glyphicon form-control-feedback glyphicon-ok");
+        await expect(page.locator("div.col-sm-10", { has: petNameTextBox }).locator('span')).toHaveClass(/glyphicon-ok/);
 
         const petBirthDate = new Date(2014, 4, 20);
         const year = petBirthDate.getFullYear();
@@ -37,69 +37,65 @@ test.describe('Date Selectors Tests', () => {
         await page.getByRole('combobox', { name: 'Type' }).selectOption('dog');
         await page.getByRole('button', { name: 'Save Pet' }).click();
 
-        const petTable = page.locator('dl.dl-horizontal');
-        const petDefinitions = petTable.getByRole('definition');
-        await expect(petDefinitions.first()).toHaveText('Tom');
-        await expect(petDefinitions.nth(1)).toHaveText(`${year}-${month}-${day}`);
-        await expect(petDefinitions.nth(2)).toHaveText('dog');
+        const tomPetSummarySection = page.locator('dl.dl-horizontal', { hasText: 'Tom' });
+        const petSummaryRows = tomPetSummarySection.getByRole('definition');
+        await expect(petSummaryRows.first()).toHaveText('Tom');
+        await expect(petSummaryRows.nth(1)).toHaveText(`${year}-${month}-${day}`);
+        await expect(petSummaryRows.nth(2)).toHaveText('dog');
 
         await page.getByRole('button', { name: 'Delete Pet' }).click();
-        await expect(petTable).toBeHidden();
+        await expect(tomPetSummarySection).toBeHidden();
     });
 
-    test(" Select the dates of visits and validate dates order", async ({ page }) => {
+    test("Select the dates of visits and validate dates order", async ({ page }) => {
         await page.getByRole('link', { name: "Jean Coleman" }).click();
-        await page.locator('app-pet-list').filter({ hasText: "Samantha" }).getByRole('button', { name: 'Add Visit' }).click();
+        const samanthaPetSummaryAndVisitsSection = page.locator('app-pet-list').filter({ hasText: "Samantha" });
+        await samanthaPetSummaryAndVisitsSection.getByRole('button', { name: 'Add Visit' }).click();
         await expect(page.getByRole('heading', { name: 'New Visit' })).toBeVisible();
 
         const petRow = page.getByRole('row', { name: 'Samantha' });
         await expect(petRow.getByRole('cell').first()).toHaveText('Samantha');
         await expect(petRow.getByRole('cell').last()).toHaveText('Jean Coleman');
 
-        const firstNewVisitDate = new Date();
-        const firstNewVisitYear = firstNewVisitDate.getFullYear();
-        const firstNewVisitMonth = String(firstNewVisitDate.getMonth() + 1).padStart(2, '0');
-        const firstNewVisitDay = String(firstNewVisitDate.getDate()).padStart(2, '0');
-        const expectedFirstNewVisitDateInputValue = `${firstNewVisitYear}/${firstNewVisitMonth}/${firstNewVisitDay}`;
+        const currentDate = new Date();
+        const currentDateVisitsTableExpectedValue = currentDate.toLocaleDateString('ja-JP', { year: 'numeric', month: '2-digit', day: '2-digit' });
+        const currentDateInputFieldExpectedValue = currentDateVisitsTableExpectedValue.replace(/\//g, "-");
 
         await page.getByRole('button', { name: 'Open Calendar' }).click();
-        await page.getByRole('button', { name: expectedFirstNewVisitDateInputValue }).click();
-        await expect(page.locator('input[name="date"]')).toHaveValue(expectedFirstNewVisitDateInputValue);
+        await page.getByRole('button', { name: currentDateVisitsTableExpectedValue }).click();
+        await expect(page.locator('input[name="date"]')).toHaveValue(currentDateVisitsTableExpectedValue);
         await page.locator('input[name="description"]').fill('dermatologists visit');
         await page.getByRole('button', { name: 'Add Visit' }).click();
 
-        const samanthaVisitsTable = page.locator('app-pet-list').filter({ hasText: "Samantha" }).locator('app-visit-list');
-        await expect(samanthaVisitsTable.getByRole('row').nth(1).getByRole('cell').first()).toHaveText(`${firstNewVisitYear}-${firstNewVisitMonth}-${firstNewVisitDay}`);
+        const samanthaVisitsTable = page.locator('app-pet-list', { hasText: "Samantha" }).locator('app-visit-list');
+        await expect(samanthaVisitsTable.getByRole('row').nth(1).getByRole('cell').first()).toHaveText(currentDateInputFieldExpectedValue);
 
         await page.locator('app-pet-list').filter({ hasText: "Samantha" }).getByRole('button', { name: 'Add Visit' }).click();
 
-        var secondNewVisitDate = new Date();
-        secondNewVisitDate.setDate(secondNewVisitDate.getDate() - 45);
-        const secondNewVisitYear = secondNewVisitDate.getFullYear();
-        const secondNewVisitMonth = String(secondNewVisitDate.getMonth() + 1).padStart(2, '0');
-        const secondNewVisitDay = String(secondNewVisitDate.getDate()).padStart(2, '0');
-        const expectedSecondNewVisitDateInputValue = `${secondNewVisitYear}/${secondNewVisitMonth}/${secondNewVisitDay}`;
+        var fortyFiveDaysAgo = new Date()
+        fortyFiveDaysAgo.setDate(currentDate.getDate() - 45);
+        const fortyFiveDaysAgoVisitsTableExpectedValue = fortyFiveDaysAgo.toLocaleDateString('ja-JP', { year: 'numeric', month: '2-digit', day: '2-digit' });
+        const fortyFiveDaysAgoInputFieldExpectedValue = fortyFiveDaysAgoVisitsTableExpectedValue.replace(/\//g, "-");
 
         await page.getByRole('button', { name: 'Open Calendar' }).click();
-        await page.getByRole('button', { name: 'Previous month' }).click();
-        const fullMonthsDifference = (firstNewVisitDate.getFullYear() - secondNewVisitDate.getFullYear()) * 12
-            + (firstNewVisitDate.getMonth() - secondNewVisitDate.getMonth()) - 1;
-        for (let i = 0; i < fullMonthsDifference; i++) {
+
+        const fortyFiveDaysAgoButton = page.getByRole('button', { name: fortyFiveDaysAgoVisitsTableExpectedValue });
+        while (!await fortyFiveDaysAgoButton.isVisible()) {
             await page.getByRole('button', { name: 'Previous month' }).click();
         }
-        await page.getByRole('button', { name: expectedSecondNewVisitDateInputValue }).click();
-        await expect(page.locator('input[name="date"]')).toHaveValue(expectedSecondNewVisitDateInputValue);
+        await fortyFiveDaysAgoButton.click();
+        await expect(page.locator('input[name="date"]')).toHaveValue(fortyFiveDaysAgoVisitsTableExpectedValue);
         await page.locator('input[name="description"]').fill('massage therapy');
         await page.getByRole('button', { name: 'Add Visit' }).click();
 
-        await expect(samanthaVisitsTable.getByRole('row').nth(1).getByRole('cell').first()).toHaveText(`${firstNewVisitYear}-${firstNewVisitMonth}-${firstNewVisitDay}`);
-        await expect(samanthaVisitsTable.getByRole('row').nth(2).getByRole('cell').first()).toHaveText(`${secondNewVisitYear}-${secondNewVisitMonth}-${secondNewVisitDay}`);
+        await expect(samanthaVisitsTable.getByRole('row').nth(1).getByRole('cell').first()).toHaveText(currentDateInputFieldExpectedValue);
+        await expect(samanthaVisitsTable.getByRole('row').nth(2).getByRole('cell').first()).toHaveText(fortyFiveDaysAgoInputFieldExpectedValue);
         const thirdLatestVisitString = await samanthaVisitsTable.getByRole('row').nth(3).getByRole('cell').first().textContent()!;
         const fourthLatestVisitString = await samanthaVisitsTable.getByRole('row').nth(4).getByRole('cell').first().textContent()!;
         const thirdLatestVisitDate = new Date(thirdLatestVisitString!);
         const fourthLatestVisitDate = new Date(fourthLatestVisitString!);
-        expect(thirdLatestVisitDate.getTime()).toBeLessThan(secondNewVisitDate.getTime());
-        expect(fourthLatestVisitDate.getTime()).toBeLessThan(firstNewVisitDate.getTime());
+        expect(thirdLatestVisitDate.getTime()).toBeLessThan(fortyFiveDaysAgo.getTime());
+        expect(fourthLatestVisitDate.getTime()).toBeLessThan(currentDate.getTime());
 
         await samanthaVisitsTable.getByRole('row', { name: "dermatologists visit" }).getByRole('button', { name: 'Delete Visit' }).click();
         await samanthaVisitsTable.getByRole('row', { name: "massage therapy" }).getByRole('button', { name: 'Delete Visit' }).click();
