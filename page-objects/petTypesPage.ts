e7f81cda61d395da2039461ad853de5396bcf53b
Roman Zakharov <romanzakharov329@gmail.com>
2025-11-20 17:08:28 +0200

    Refactor inputFields tests

--- page-objects/petTypesPage.ts
+++ page-objects/petTypesPage.ts
@@ -4,26 +4,52 @@ import { HelperBase } from './helperBase';
 export class PetTypesPage extends HelperBase {
     async addNewPetType(petTypeName: string) {
         await test.step(`Add new pet type ${petTypeName}`, async () => {
-            const lastTableTextBox = this.page.getByRole("table").getByRole('textbox').last();
             await this.page.getByRole('button', { name: 'Add' }).click();
             await expect(this.page.getByRole('heading', { name: 'New Pet Type' })).toBeVisible();
             await expect(this.page.locator('label', { hasText: 'Name' })).toBeVisible();
             await expect(this.page.locator('input#name')).toBeVisible();
             await this.page.locator('input#name').fill(petTypeName);
             await this.page.getByRole('button', { name: 'Save' }).click();
-            await expect(lastTableTextBox).toHaveValue(petTypeName);
+            await expect(this.page.getByRole("table").getByRole('textbox').last()).toHaveValue(petTypeName);
         });
     }
 
     async deletePetType(petTypeName: string) {
         await test.step(`Delete the pet type ${petTypeName}`, async () => {
-            const lastTableTextBox = this.page.getByRole("table").getByRole('textbox').last();
             this.page.on('dialog', async dialog => {
                 expect(dialog.message()).toContain('Delete the pet type?');
                 await dialog.accept();
             });
             await this.page.getByRole('button', { name: 'Delete' }).last().click();
-            await expect(lastTableTextBox).not.toHaveValue(petTypeName);
+            await expect(this.page.getByRole("table").getByRole('textbox').last()).not.toHaveValue(petTypeName);
         });
+    }
+
+    async goToEditPetTypeByPressingEditButton(petTypeName: string) {
+        await this.page.getByRole('row', { name: petTypeName }).getByRole('button', { name: 'Edit' }).click();
+        await expect(this.page.getByRole('heading', { name: 'Edit Pet Type' })).toBeVisible();
+        await expect(this.page.locator('#name')).toHaveValue(petTypeName);
+    }
+
+    async editPetNameAndVerifyChangeOnPage(newName: string) {
+        await this.fillPetTypeNameInputField(newName);
+        await this.page.getByRole('button', { name: 'Update' }).click();
+        await expect(this.page.locator('[id="0"]')).toHaveValue(newName);
+    }
+
+    async fillPetTypeNameInputField(petTypeName: string) {
+        const petTypeInputField = this.page.locator('#name');
+        await petTypeInputField.fill(petTypeName);
+        await expect(petTypeInputField).toHaveValue(petTypeName);
+    }
+
+    async clearPetTypeNameInputFieldAndVerifyTheTip() {
+        await (this.page.locator('#name')).clear();
+        await expect(this.page.locator('span.help-block')).toHaveText('Name is required');
+    }
+
+    async cancelChangingPetAndPetTypesPageIsDisplayed() {
+        await this.page.getByRole('button', { name: 'Cancel' }).click();
+        await expect(this.page.getByRole('heading', { name: 'Pet Types' })).toBeVisible();
     }
 }
