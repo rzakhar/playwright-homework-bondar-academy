9dc8c175be12cb803dd12f5319ed9bd042e244d3
Roman Zakharov <romanzakharov329@gmail.com>
2025-11-25 11:19:20 +0200

    Add an additional check

--- tests/workingWithApi.spec.ts
+++ tests/workingWithApi.spec.ts
@@ -80,11 +80,12 @@ test('Add and delete an owner', async ({ page, request }) => {
     const newOwnerId = newOwnerResponseBody.id;
 
     await pm.navigateTo().ownersSearchPage();
+    await pm.onOwnersPage().verifyOwnersTableRowsCount(11);
     await pm.onOwnersPage().verifyOwnerInTable('TestFirstName TestLastName', '123 Test St', 'TestCity', '1234567890');
 
-    const deleteOwnerResponse = await request.delete(`https://petclinic-api.bondaracademy.com/petclinic/api/owners/${newOwnerId}`, {
-    });
+    const deleteOwnerResponse = await request.delete(`https://petclinic-api.bondaracademy.com/petclinic/api/owners/${newOwnerId}`);
     expect(deleteOwnerResponse.status()).toEqual(204);
     await page.reload();
+    await pm.onOwnersPage().verifyOwnersTableRowsCount(10);
     await pm.onOwnersPage().verifyOwnerIsNotInTable('TestFirstName TestLastName');
-
+});
