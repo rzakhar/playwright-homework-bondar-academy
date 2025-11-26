edf6e4f1523e8d633d362bed1ad8c51737c945e1
Roman Zakharov <romanzakharov329@gmail.com>
2025-11-26 20:09:49 +0200

    Update addNewOwnerPage.ts

--- page-objects/addNewOwnerPage.ts
+++ page-objects/addNewOwnerPage.ts
@@ -3,13 +3,6 @@ import { HelperBase } from './helperBase';
 
 export class AddNewOwnerPage extends HelperBase {
 
-    /** 
-     * Click the "Add Owner" button to submit the new owner form
-     */
-    async clickAddOwner() {
-        await this.page.getByRole('button', { name: 'Add Owner' }).click();
-    }
-
     /**
      * Fill in the new owner details form
      * @param firstName Owner's first name
@@ -17,12 +10,19 @@ export class AddNewOwnerPage extends HelperBase {
      * @param address Owner's address
      * @param city Owner's city
      * @param telephone Owner's telephone
-    */
+     */
     async fillNewOwnerDetails(firstName: string, lastName: string, address: string, city: string, telephone: string) {
         await this.page.getByRole('textbox', { name: 'First Name' }).fill(firstName);
         await this.page.getByRole('textbox', { name: 'Last Name' }).fill(lastName);
         await this.page.getByRole('textbox', { name: 'Address' }).fill(address);
         await this.page.getByRole('textbox', { name: 'City' }).fill(city);
         await this.page.getByRole('textbox', { name: 'Telephone' }).fill(telephone);
+    }
+
+    /** 
+     * Click the "Add Owner" button to submit the new owner form
+     */
+    async clickAddOwner() {
+        await this.page.getByRole('button', { name: 'Add Owner' }).click();
     }
 }
